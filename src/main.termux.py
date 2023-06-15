123c29f15289785603799425c699632bd26b0b5d
ignxcy <ignacyy@proton.me>
2023-06-15 16:53:56 +0200

    add -v and --version args

--- src/main.termux.py
+++ src/main.termux.py
@@ -1,6 +1,8 @@
 #!/data/data/com.termux/files/usr/bin/python3
 import distro
 import subprocess
+import sys
+import os
 
 black = '\033[0;30m'
 red = '\033[0;31m'
@@ -12,9 +14,13 @@ cyan = '\033[0;36m'
 white = '\033[0;37m'
 NC = '\033[0m'
 
-print("    .--.")
-print(f"   |o_o |    {purple}Distro:    {NC}{distro.name()}")
-print(f"   |:_/ |    {blue}Kernel:    {NC}{subprocess.getoutput('uname -s -r')}")
-print(f"  //   \ \   {green}Username:  {NC}{subprocess.getoutput('whoami')}")
-print(f"/'\_   _/`\\  {red}Hostname:  {NC}{subprocess.getoutput('hostname')}")
-print(f"\\___)=(___/")
+if len(sys.argv) > 1 and sys.argv[1] == "-v" or len(sys.argv) > 1 and sys.argv[1] == "--version":
+    with open("./version.txt") as v:
+        print(v.read().strip())
+else:
+    print("    .--.")
+    print(f"   |o_o |    {purple}Distro:    {NC}{distro.name()}")
+    print(f"   |:_/ |    {blue}Kernel:    {NC}{subprocess.getoutput('uname -s -r')}")
+    print(f"  //   \ \   {green}Username:  {NC}{subprocess.getoutput('whoami')}")
+    print(f"/'\_   _/`\\  {red}Hostname:  {NC}{subprocess.getoutput('hostname')}")
+    print(f"\\___)=(___/")
