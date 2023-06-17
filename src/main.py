363c21138c73860f2cf4ccd99e6a0332937c4067
ignxcy <ignacyy@proton.me>
2023-06-17 22:20:11 +0200

    changed argv == arg to arg in argv in main.py

--- src/main.py
+++ src/main.py
@@ -17,12 +17,12 @@ NC = '\033[0m'
 if len(sys.argv) > 1 and sys.argv[1] == "-v" or len(sys.argv) > 1 and sys.argv[1] == "--version":
     with open("./version.txt") as v:
         print(v.read().strip())
-elif len(sys.argv) > 1 and sys.argv[1] == "--noasciiart":
+elif len(sys.argv) > 1 and "--noasciiart" in sys.argv[1]:
     print(f"{purple}Distro:{NC} {distro.name()}")
     print(f"{blue}Kernel: {NC}{subprocess.getoutput('uname -s -r')}")
     print(f"{green}Username: {NC}{subprocess.getoutput('whoami')}")
     print(f"{red}Hostname: {NC}{subprocess.getoutput('hostname')}")
-elif len(sys.argv) > 1 and sys.argv[1] == "--asciiartonly":
+elif len(sys.argv) > 1 and "--asciiartonly" in sys.argv[1]:
     print("    .--.")
     print(f"   |o_o |")
     print(f"   |:_/ |")
