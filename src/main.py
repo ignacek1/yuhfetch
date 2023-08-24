21433592e189ca686a4d9e51c3c0f5129939e68c
ignxcy <ignacyy@proton.me>
2023-08-24 13:25:45 +0200

    add help arg

--- src/main.py
+++ src/main.py
@@ -33,6 +33,10 @@ elif len(sys.argv) > 1 and "--asciiartonly" in sys.argv[1]:
     print(f"  //   \ \ ")
     print(f"/'\_   _/`\\ ")
     print(f"\\{yellow}___{NC})=({yellow}___{NC}/")
+elif len(sys.argv) > 1 and "--help" or len(sys.argv) > 1 and "-h":
+    print("""-v and --version: prints the version, needs yuhfetch to be installed with make or be in the yuhfetch's src directory.
+--noasciiart: prints the data with no ascii art
+--asciiartonly: prints only the ascii art with no data""")
 else:
     print("    .--.")
     print(f"   |o_o |    {purple}Distro:    {NC}{distro.name()}")
